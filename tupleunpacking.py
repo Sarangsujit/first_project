5423ccb334534657f1a06d11941aba131c4391ff
Sarangsujit <110896790+Sarangsujit@users.noreply.github.com>
2022-08-09 15:24:14 +0530

    Add files via upload

---
+++ tupleunpacking.py
@@ -0,0 +1,25 @@
+# fruits=("apple","banana","cherry","guava","mango","pineapple")
+# a,b,*c=fruits
+# print(a)
+# print(b)
+# print(c)
+
+
+
+# ttuple=(1,2,3,4)
+# ttuple=list(ttuple)
+# ttuple.append(5)
+# ttuple=tuple(ttuple)
+# print(ttuple)
+
+
+set={1,2,3,4,5,5,5}
+print(set)
+set.remove(1)
+print(set)
+set.discard(2)
+print(set)
+set.pop()
+print(set)
+set.clear()
+print(set)
