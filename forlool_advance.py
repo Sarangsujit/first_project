ea4e1dbc610c3f8cc4a2a3287c396c4d3ae36f67
Sarangsujit <110896790+Sarangsujit@users.noreply.github.com>
2022-08-09 15:04:44 +0530

    first commit

---
+++ forlool_advance.py
@@ -0,0 +1,13 @@
+# mul=1
+# for i in range(1,11):
+#     mul*=i
+# print(mul)
+
+num=12345
+sum=0
+for i in range(len(str(num))):  #convert num to string and find its length
+    temp=num%10 #to get the last character
+    num=num//10 #to avoid the last character
+    # print(num)
+    sum+=temp  #
+print(sum)
